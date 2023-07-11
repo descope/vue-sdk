b6e26d3a150092484789836fe44bb806a4f76f39
Nir Gur Arie <nir@descope.com>
2023-07-12 00:33:39 +0300

    migrate Descope to setup script

--- src/Descope.vue
+++ src/Descope.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
 	<div>
 		<descope-wc
@@ -16,65 +17,49 @@
 	</div>
 </template>
 
-<script lang="ts">
-import DescopeWc from '@descope/web-component';
+<script setup lang="ts">
+import DescopeWcClass from '@descope/web-component';
 import { useOptions, useDescope } from './hooks';
 import { baseHeaders } from './constants';
 import { RequestConfig } from '@descope/core-js-sdk';
-import { SetupContext } from 'vue';
 
-DescopeWc.sdkConfigOverrides = { baseHeaders };
-
-export default {
-	// eslint-disable-next-line vue/multi-word-component-names
-	name: 'Descope',
-	props: {
-		flowId: {
-			type: String,
-			required: true
-		},
-		tenant: {
-			type: String
-		},
-		theme: {
-			type: String
-		},
-		debug: {
-			type: Boolean
-		},
-		telemetryKey: {
-			type: String
-		},
-		redirectUrl: {
-			type: String
-		},
-		autoFocus: {
-			type: Boolean
-		}
+DescopeWcClass.sdkConfigOverrides = { baseHeaders };
+defineProps({
+	flowId: {
+		type: String,
+		required: true
 	},
-	emits: ['success', 'error'],
-	setup(_: unknown, { emit }: SetupContext) {
-		const { projectId, baseUrl, sessionTokenViaCookie } = useOptions();
-		const sdk = useDescope();
-
-		const onSuccess = async (e: CustomEvent) => {
-			// Note: We need to emit AFTER the afterRequest hook has been called, but for
-			// an unknown reason, the emit is not called if we await the hook.
-			emit('success', e);
-			await sdk.httpClient.hooks?.afterRequest?.(
-				{} as RequestConfig,
-				new Response(JSON.stringify(e.detail))
-			);
-		};
-		const onError = (e: Event) => emit('error', e);
-
-		return {
-			projectId,
-			baseUrl,
-			sessionTokenViaCookie,
-			onSuccess,
-			onError
-		};
+	tenant: {
+		type: String
+	},
+	theme: {
+		type: String
+	},
+	debug: {
+		type: Boolean
+	},
+	telemetryKey: {
+		type: String
+	},
+	redirectUrl: {
+		type: String
+	},
+	autoFocus: {
+		type: Boolean
 	}
+});
+const emit = defineEmits(['success', 'error']);
+const { projectId, baseUrl } = useOptions();
+const sdk = useDescope();
+
+const onSuccess = async (e: CustomEvent) => {
+	// Note: We need to emit AFTER the afterRequest hook has been called, but for
+	// an unknown reason, the emit is not called if we await the hook.
+	emit('success', e);
+	await sdk.httpClient.hooks?.afterRequest?.(
+		{} as RequestConfig,
+		new Response(JSON.stringify(e.detail))
+	);
 };
+const onError = (e: Event) => emit('error', e);
 </script>
