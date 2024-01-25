2e02410d71d244b20a2de72c660bd89ebae5e984
Asaf Shen <asaf@descope.com>
2024-01-25 20:20:32 +0200

    remove prettier rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -27,8 +27,7 @@ module.exports = {
 	},
 	rules: {
 		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-		'prettier/prettier': 'error'
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
 	},
 	overrides: [
 		{
