c6ec4b1bd542e714bb841e645a67630fac585da8
Itai Hanski <itai@descope.com>
2024-04-10 15:14:30 +0300

    Add session and refresh token expiration utilities (#71)

--- src/sdk.ts
+++ src/sdk.ts
@@ -46,6 +46,12 @@ export const getRefreshToken = () => {
 	return '';
 };
 
+export const isSessionTokenExpired = (token = getSessionToken()) =>
+	globalSdk?.isJwtExpired(token);
+
+export const isRefreshTokenExpired = (token = getRefreshToken()) =>
+	globalSdk?.isJwtExpired(token);
+
 export const getJwtPermissions = wrapInTry(
 	(token = getSessionToken(), tenant?: string) =>
 		globalSdk?.getJwtPermissions(token, tenant)
